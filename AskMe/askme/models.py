f81578c8ddb1ad7e7086803dc53ce46a4731998b
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2023-04-10 21:46:20 +0300

    First instance of DataBase in models.py

--- AskMe/askme/models.py
+++ AskMe/askme/models.py
@@ -1,3 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import User
+
 QUESTIONS = [
     {
         'id': i,
@@ -29,3 +32,57 @@ QUESTION_TAGS = [
         'tag_name': f'tag_{i}',
     } for i in range(4)
 ]
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(
+        'Profile',
+        on_delete=models.PROTECT
+    )
+    tag = models.ManyToManyField('Tag')
+
+
+class QuestionLikes(models.Model):
+    question = models.ForeignKey(
+        'Question',
+        on_delete=models.PROTECT
+    )
+    value = models.IntegerField()
+
+
+class Answer(models.Model):
+    content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    correct_flag = models.BooleanField()
+    rating = models.IntegerField()
+    question = models.ForeignKey(
+        'Question',
+        on_delete=models.PROTECT
+    )
+    author = models.ForeignKey(
+        'Profile',
+        on_delete=models.PROTECT
+    )
+
+
+class AnswerLike(models.Model):
+    answer = models.ForeignKey(
+        'Answer',
+        on_delete=models.PROTECT
+    )
+    value = models.IntegerField()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # avatar
+    signup_date = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField()
+
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=30)
